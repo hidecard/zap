1dc45d8b3576cee11cdf022367e91dfa880726f4
hidecard <7.6858518e+07+hidecard@users.noreply.github.com>
2026-08-21 04:47:31 +0000

    Fix Windows async process output fixture

--- native/src/async_runtime.rs
+++ native/src/async_runtime.rs
@@ -1246,7 +1246,7 @@ mod tests {
         #[cfg(windows)]
         let (command, arguments) = (
             "cmd".to_owned(),
-            vec!["/C".to_owned(), "echo zap".to_owned()],
+            vec!["/C".to_owned(), "<nul set /p =zap".to_owned()],
         );
         #[cfg(not(windows))]
         let (command, arguments) = (
@@ -1272,7 +1272,7 @@ mod tests {
         #[cfg(windows)]
         let (command, arguments) = (
             "cmd".to_owned(),
-            vec!["/C".to_owned(), "echo zap".to_owned()],
+            vec!["/C".to_owned(), "<nul set /p =zap".to_owned()],
         );
         #[cfg(not(windows))]
         let (command, arguments) = (
